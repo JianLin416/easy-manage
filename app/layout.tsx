5c49ab57915b8e5e7bb7009642a26e163d1a5041
moreThanMTE <lijianlin050416@gmail.com>
2024-11-03 10:05:02 +0800

    daily work updated header

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,25 +1,28 @@
-import type { Metadata } from "next";
+import type {Metadata} from "next";
 import "@/public/globals.css";
 import SideBar from "./components/SideBar";
 import Header from "./components/Header";
 
 export const metadata: Metadata = {
   title: "easyManage",
-  description: "made for my self-learning",
+  description: "created for self-learning",
+  icons: {icon: './favicon.ico'},
 };
 
 export default function RootLayout({
-  children,
-}: Readonly<{
+                                     children,
+                                   }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
-      <body className="h-screen w-screen bg-gray-200 text-neutral-950">
-				<Header />
-        <SideBar />
-        {children}
-      </body>
+    <body className="bg-gray-200 text-neutral-950 font-semibold">
+    <Header/>
+    <div className="w-10/12 mx-auto">
+      <SideBar/>
+      {children}
+    </div>
+    </body>
     </html>
   );
 }
