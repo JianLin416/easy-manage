2d4d7bee468f758b8a65f432de3a86176b116caf
moreThanMTE <lijianlin050416@gmail.com>
2024-11-08 08:42:58 +0800

    daily update

---
+++ app/components/Pagination.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+
+export default function Pagination ({
+  currentPage,
+  totalPages,
+  changePage,
+}: {
+  currentPage: number,
+  totalPages: number,
+  changePage: (page: number) => Promise<void>
+}) {
+  const handlePrevPage = async () => {
+    if (currentPage > 1) {
+      await changePage(currentPage - 1);
+    }
+  };
+
+  const handleNextPage = async () => {
+    if (currentPage < totalPages) {
+      await changePage(currentPage + 1);
+    }
+  };
+
+  return (
+    <div className="w-full flex justify-center mt-5">
+      <button onClick={handlePrevPage} disabled={currentPage === 1}>
+        上一页
+      </button>
+      <span className='mx-5'>
+        第 {currentPage} 页，共 {totalPages} 页
+      </span>
+      <button onClick={handleNextPage} disabled={currentPage === totalPages}>
+        下一页
+      </button>
+    </div>
+  );
+};
--- app/components/studentInfo/index.tsx
+++ app/components/studentInfo/index.tsx
@@ -1,7 +1,8 @@
 import axios from 'axios'
-import {useEffect, useState} from 'react'
-import {Student} from "@prisma/client";
-import React from 'react';
+import { useEffect, useState } from 'react'
+import { Student } from '@prisma/client'
+import React from 'react'
+import Pagination from '@/app/components/Pagination'
 
 export default function StudentInfo() {
 
@@ -25,14 +26,19 @@ export default function StudentInfo() {
 
   return (
     <>
-      <StudentTable students={students} />
+      <p>搜索指定学生</p>
+      <input placeholder='输入名字'/>
+      <input placeholder='输入学号'/>
+      <button onClick={() => console.log(666)}>查询</button>
+      <StudentTable students={students}/>
+      <Pagination currentPage={pagination.currentPage} totalPages={pagination.totalPages} changePage={getAllStudents}/>
     </>
   )
 }
 
-function StudentTable({ students }: { students: Student[] }) {
+function StudentTable({students}: { students: Student[] }) {
   return (
-    <table className='table-auto w-full'>
+    <table className='table-auto w-full mx-auto'>
       <thead className='bg-yellow-100'>
         <tr className='h-10'>
           <th>系部</th>
@@ -46,21 +52,21 @@ function StudentTable({ students }: { students: Student[] }) {
       </thead>
       <tbody className='text-center'>
 
-      {students.map(student => (
-        <tr key={student.student_id} className='h-12'>
-          <td>{student.student_department}</td>
-          <td>{student.student_major}</td>
-          <td>{student.class_name}</td>
-          <td>{student.student_name}</td>
-          <td>{student.student_gender}</td>
-          <td>{student.student_number}</td>
-          <td>
-            <button className='hover:text-indigo-700'>详情</button>
-          </td>
-        </tr>
-      ))}
+        {students.map(student => (
+          <tr key={student.student_id} className='h-12'>
+            <td>{student.student_department}</td>
+            <td>{student.student_major}</td>
+            <td>{student.class_name}</td>
+            <td>{student.student_name}</td>
+            <td>{student.student_gender === 'man'? '男': '女'}</td>
+            <td>{student.student_number}</td>
+            <td>
+              <button className='mr-3.5 hover:text-red-700'>操作</button>
+              <button className='hover:text-indigo-700'>详情</button>
+            </td>
+          </tr>
+        ))}
       </tbody>
     </table>
   )
 }
-
