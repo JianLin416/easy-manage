457e785ba3d63d42c889601211ed9e9d50c7cf53
moreThanMTE <lijianlin050416@gmail.com>
2024-11-15 10:29:44 +0800

    修复token失效无法自动跳转的bug

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -2,11 +2,11 @@
 
 import Logo from "@/public/Logo"
 import Link from "next/link"
-import { jwtDecode } from "jwt-decode"
 import { usePathname, useRouter } from "next/navigation"
 import { useEffect, useState } from "react"
+import { myAxios } from "@/app/api/axios"
 
-export default function Header () {
+export default function Header() {
   type UserRole = 'admin' | 'teacher' | 'guider'
 
   const roleMap: Record<UserRole, string> = {
@@ -16,19 +16,32 @@ export default function Header () {
   }
   const pathname = usePathname()
   const router = useRouter()
-  const [decodeToken, setDecodeToken] = useState<any>(null)
+  const [decodeToken, setDecodeToken] = useState<{ user_role: UserRole; user_name: string } | null>(null)
 
   useEffect(() => {
-    const token = localStorage.getItem("token")
-    if (token) {
-      const decoded = jwtDecode(token)
-      setDecodeToken(decoded)
-    } else {
-      if (pathname === '/') router.push("/user/login")
+    async function getUserInfo() {
+      const token = localStorage.getItem('token')
+      if (!token) {
+        if (pathname === '/') router.push('/user/login')
+        return
+      }
+      try {
+        const response = await myAxios.post('/api/user/getInfo', {})
+        if (response.data.status === 'error') {
+          router.push('/user/login')
+        } else {
+          setDecodeToken(response.data.data)
+        }
+      } catch (error) {
+        console.error('Error fetching user info:', error)
+        router.push('/user/login')
+      }
     }
-  }, [])
 
-  function logout () {
+    getUserInfo()
+  }, [pathname, router])
+
+  function logout() {
     localStorage.removeItem("token")
     window.location.reload()
   }
@@ -37,12 +50,12 @@ export default function Header () {
     <div className='w-full h-16 shadow'>
       <div className='w-9/12 h-full mx-auto flex items-center'>
         <Link href={'/'} className='flex items-center'>
-          <Logo height={40}/>
+          <Logo height={40} />
           <p className='ml-4 text-lg font-bold'>easyManage</p>
         </Link>
         {decodeToken ? (
           <div className='ml-auto'>
-          <a className='cursor-pointer'>【{roleMap[decodeToken.user_role as UserRole]}】</a>
+            <a className='cursor-pointer'>【{roleMap[decodeToken.user_role]}】</a>
             <a className='cursor-pointer mx-5'>{decodeToken.user_name}</a>
             <a className='cursor-pointer' onClick={logout}>退出登陆</a>
           </div>
