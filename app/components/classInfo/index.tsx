3a8efd41f1f802df89d38cf5442065b2662585cb
moreThanMTE <lijianlin050416@gmail.com>
2024-11-21 09:25:21 +0800

    移除多余代码

--- app/components/classInfo/index.tsx
+++ app/components/classInfo/index.tsx
@@ -1,12 +1,12 @@
 import { myAxios } from '@/app/api/axios'
 import { useEffect, useState } from 'react'
-import { Renamedclass } from '@prisma/client'
+import { Class } from '@prisma/client'
 import React from 'react'
 import Pagination from '@/app/components/Pagination'
 
 export default function ClassInfo() {
 
-  const [renamedClasses, setRenamedClasses] = useState<Renamedclass[]>([])
+  const [renamedClasses, setRenamedClasses] = useState<Class[]>([])
   const [pagination, setPagination] = useState({
     currentPage: 0,
     totalPages: 0,
@@ -14,13 +14,11 @@ export default function ClassInfo() {
     limit: 0
   })
   const [c_name, setC_name] = useState('')
-  const [departments, setDepartments] = useState([''])
 
   async function getAllClasses(page: number = 1, limit: number = 20) {
     const response = await myAxios.get(`/api/class/getAllClass?page=${page}&limit=${limit}`)
     setRenamedClasses(response.data.data.classes)
     setPagination(response.data.data.pagination)
-    setDepartments(response.data.data.departments)
   }
 
   async function getClass(name: string) {
@@ -33,11 +31,6 @@ export default function ClassInfo() {
 
   type UserRole = 'admin' | 'teacher' | 'guider'
 
-  const roleMap: Record<UserRole, string> = {
-    admin: '管理员',
-    teacher: '班主任',
-    guider: '导员',
-  }
   const [decodeToken, setDecodeToken] = useState<{ user_role: UserRole; user_name: string } | null>(null)
 
   useEffect(() => {
